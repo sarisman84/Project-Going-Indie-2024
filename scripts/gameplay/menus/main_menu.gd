88a8e35e837dee4c9f3f2348658e4ab46841bc72
TylerMooney <71684071+TylerMooney@users.noreply.github.com>
2024-05-06 04:04:19 -0500

    Add wasd and controller ui input
    I've made it so that you can navigate the main menu with wasd and controller input

--- scripts/gameplay/menus/main_menu.gd
+++ scripts/gameplay/menus/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://nodes/scenes/test_level.tscn")
