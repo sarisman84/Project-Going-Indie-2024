f4126edaa9adaefac67bacd55fdbc6c1e5db6ea5
Spyridon Passas <spyridonpassas0325@gmail.com>
2024-05-07 23:18:35 +0200

    Bugfixing. Reworked how rails are created.
    Reworked how one creates rails!
    - User now only requires to set a path from a 3D curve and to set the bounding box
    
    Did a number of bugfixes:
    - Fixed a bug that caused the player to traverse in the wrong direction when jumping from certain angles.
    - Fixed a bug that caused the player to get stuck in an infinite loop should the player stop grinding inside a vertical rail
    
    Reworked internal logic to work with the new tool
    - Implemented a system that creates a shape at the start of runtime with a determine size and position set by the rail.
    - Added some visual elements to help align the bounding box.

--- scripts/gameplay/player/player_controller.gd
+++ scripts/gameplay/player/player_controller.gd
@@ -1,7 +1,8 @@
+@tool
 class_name PlayerController
 extends CharacterBody3D
 
-enum States {Idle, Moving, Airborne, Grinding }
+enum States {Idle, Moving, Airborne, Grinding, StoppedGrinding }
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -11,21 +12,33 @@ var jumpFlag : bool
 
 @onready var camera = $camera_anchor
 
+@export_category("General Settings")
 @export var movementSpeed : float = 5.0
 @export var boostSpeed : float = 10.0
 @export var jumpHeight : float = 1.0
+@export_category("Fall Settings")
 @export var fallMultiplier : float = 1.0
 @export var lowJumpFallMultiplier : float = 1.2
+@export_category("Acceleration Settings")
 @export var groundAcceleration : float = 1.0
 @export var groundDecceleration : float = 1.0
 @export var airAcceleration : float = 1.0
 @export var airDecceleration : float = 1.0
 @export var slideDecceleration : float = 0.8
-
+@export_category("Rail Grind Settings")
+@export var railDetectionRadius : float = 0.5
+@export var railGrindCooldownInSeconds : float = 0.25
+@export_category("Other")
 @export var cameraOffset : Vector3
 @export var model : Node3D
 
-var currentState : States
+
+var currentState : States:
+	set(new_value):
+		if new_value == States.StoppedGrinding:
+			currentCooldown = railGrindCooldownInSeconds
+		currentState = new_value
+var currentCooldown : float
 
 func set_default_controls(newState):
 	defaultControls = newState
@@ -47,7 +60,7 @@ func _physics_process(delta):
 		velocity.y = get_jump_velocity(jumpHeight)
 	
 	# Get the user input
-	var input_dir : Vector3
+	var input_dir := Vector3.ZERO
 	input_dir.x = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) 
 	input_dir.z = (Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward"))
 	
@@ -78,19 +91,27 @@ func _physics_process(delta):
 	if model and velocity.length() > 0.2:
 		#var local_dir = Vector2(velocity.z, velocity.x)
 		#model.rotation.y = local_dir.angle()
-		rotate_model_towards(dir, up_direction)
+		var horizontalVel = Vector3(velocity.x, 0, velocity.z)
+		rotate_model_towards(horizontalVel, up_direction)
 	
 	# Apply calculations
 	move_and_slide()
 	
-func _process(delta):
+func _process(_delta):
 	camera.position = position + cameraOffset
-	update_states()
+	update_states(_delta)
+	DebugDraw3D.draw_sphere(position, railDetectionRadius, Color.MAGENTA)
 
 func get_jump_velocity(inputHeight : float):
 	return sqrt(2.0 * gravity * inputHeight)
 	
-func update_states():
+func update_states(delta):
+	if currentState == States.StoppedGrinding:
+		currentCooldown -= delta
+		if currentCooldown < 0:
+			currentState = States.Idle
+		return
+	
 	if is_on_floor():
 		if velocity.length() > 2.0:
 			currentState = States.Moving
@@ -106,6 +127,8 @@ func get_current_speed():
 	return speed
 
 func rotate_model_towards(forwardDir : Vector3 ,upDir : Vector3 = Vector3.UP):
+	if forwardDir == Vector3.ZERO:
+		return
 	model.look_at(transform.origin - forwardDir, upDir)
 
 	
--- scripts/gameplay/rail_section.gd
+++ scripts/gameplay/rail_section.gd
@@ -1,50 +1,40 @@
+@tool
 extends Path3D
 
-@export var hitbox : Area3D
+@export var hitboxAABB : AABB
+@onready var model = $model
+@onready var hitbox_collider = $hitbox/hitbox_collider
+@onready var hitbox = $hitbox
 
 var isPlayerDetected : bool
 var playerDirection : float
 var player : PlayerController
+var hasPlayerStartedGrinding : bool
 
+
+func get_global_aabb_center():
+	return hitboxAABB.get_center() + position - (hitboxAABB.size / 2.0)
+
+func get_global_aabb_start():
+	return hitboxAABB.position + position - (hitboxAABB.size / 2.0)
+	
+func get_global_aabb_end():
+	return hitboxAABB.end + position - (hitboxAABB.size / 2.0)
 
 func _ready():
+	hitbox_collider.global_position = get_global_aabb_center()
+	var box_shape := BoxShape3D.new()
+	box_shape.size = hitboxAABB.size
+	hitbox_collider.shape = box_shape
+	
 	hitbox.connect("body_entered", _on_hitbox_body_entered)
 	hitbox.connect("body_exited", _on_hitbox_body_exited)
 
-		
-func _physics_process(delta):
-	if not isPlayerDetected:
-		return
-	var offset = get_closest_offset(player.position)
-	var pos = curve.sample_baked(offset, true)
-	
-	var playerDir = player.velocity.normalized()
-	
-	var forward = pos.direction_to(curve.sample_baked(offset + playerDirection, true))
-	var up = curve.sample_baked_up_vector(offset, true)
-	
-	if forward == Vector3.ZERO:
-		_on_hitbox_body_exited(player)
-		return
-	
-	player.velocity = forward * player.get_current_speed()
-	player.move_and_slide()
-	player.rotate_model_towards(forward, up)
-
-	DebugDraw3D.draw_sphere(pos + position, 0.15, Color.YELLOW)
-	DebugDraw3D.draw_arrow_ray(pos + position, up, 2, Color.GREEN, 0.15)
-	DebugDraw3D.draw_arrow_ray(pos + position, forward, 0.25 * player.get_current_speed(), Color.BLUE, 0.15)
-	
-	
-# Assuming the player is masked to layer 2,
-# it should only detect the player
-func _on_hitbox_body_entered(body):
-	player = body as PlayerController
-	
+# Starts the grind state
+func on_grind_start():
 	if player.currentState == PlayerController.States.Grinding:
 		return
 	player.currentState = PlayerController.States.Grinding
-	
 	var offset = get_closest_offset(player.position)
 	var up = curve.sample_baked_up_vector(offset, true)
 	
@@ -56,21 +46,90 @@ func _on_hitbox_body_entered(body):
 	var endingRailPos = position + curve.get_point_position(get_next_closest_point_index(player.position))
 
 	var dirToEnd = (endingRailPos - player.position).normalized()
-	
-	var playerVelDir = player.velocity.normalized()
 
-	if playerVelDir.dot(dirToEnd) > 0.9:
+	var playerVelDir = player.velocity.normalized()
+	
+	DebugDraw3D.draw_arrow_ray(player.position, playerVelDir,1.0, Color.BLUE,0.15, false, 10.0)
+	DebugDraw3D.draw_arrow_ray(player.position, dirToEnd,1.0, Color.RED,0.15, false, 10.0)
+
+	if playerVelDir.dot(dirToEnd) > 0.0:
 		playerDirection = 0.1
 	else:
 		playerDirection = -0.1
+	print("Start grinding")
 
+# Ends the grind state
+func on_grind_end():
+	player.up_direction = Vector3.UP
+	player.set_default_controls(true)
+	player.move_and_slide()
+	print("Stop grinding")
+	player.currentState = PlayerController.States.StoppedGrinding
+	
+func _process(_delta):
+	DebugDraw3D.draw_aabb_ab(get_global_aabb_start(), get_global_aabb_end(),Color.CYAN)
+	DebugDraw3D.draw_sphere(get_global_aabb_center(),0.15, Color.YELLOW)
+	
+	if not isPlayerDetected:
+		return
+	if player.currentState == PlayerController.States.StoppedGrinding:
+		return
+	if is_player_close_to_curve() and player.currentState != PlayerController.States.Grinding:
+		on_grind_start()
+	
+# Checks if the player is close enough to the rail
+func is_player_close_to_curve():
+	var offset = get_closest_offset(player.position)
+	
+	var closestPosition = curve.sample_baked(offset,true) + position
+	DebugDraw3D.draw_sphere(closestPosition, 0.15, Color.CHARTREUSE)
+	var dist = (closestPosition - player.position).length()
+	return dist < player.railDetectionRadius / 2.0
+		
+func _physics_process(_delta):
+	if not isPlayerDetected:
+		return
+	if not player.currentState == PlayerController.States.Grinding:
+		return
+	# Get the current curve distance from the player position
+	var offset = get_closest_offset(player.position)
+	# Sample a position based off the distance
+	var pos = curve.sample_baked(offset, true)
+	
+	var _playerDir = player.velocity.normalized()
+	
+	# Get the forward direction from a sample with the user's intented direction
+	var forward = pos.direction_to(curve.sample_baked(offset + playerDirection, true))
+	# Sample the normal of the curve
+	var up = curve.sample_baked_up_vector(offset, true)
+	
+	# If the forward direction is zero, it means that we have reached the end of the curve.
+	if forward == Vector3.ZERO:
+		on_grind_end()
+		return
+	
+	# Apply some physics velocity and rotation based on the above calculations
+	player.velocity = forward * player.get_current_speed()
+	player.move_and_slide()
+	player.rotate_model_towards(forward, up)
+
+	# Draw the calculated info
+	DebugDraw3D.draw_sphere(pos + position, 0.15, Color.YELLOW)
+	DebugDraw3D.draw_arrow_ray(pos + position, up, 2, Color.GREEN, 0.15)
+	DebugDraw3D.draw_arrow_ray(pos + position, forward, 0.25 * player.get_current_speed(), Color.BLUE, 0.15)
+	
+	
+# Assuming the player is masked to layer 2,
+# it should only detect the player
+func _on_hitbox_body_entered(body):		
+	player = body as PlayerController
+	isPlayerDetected = true
 
 # Same as above!
 func _on_hitbox_body_exited(body):
-	player = body as PlayerController
-	player.up_direction = Vector3.UP
 	isPlayerDetected = false
-	player.set_default_controls(true)
+	DebugDraw3D.draw_sphere(body.position, 0.15, Color.RED, 10)
+	pass
 
 func get_closest_offset(globalPosition : Vector3):
 	var localPosition = globalPosition - position
@@ -96,6 +155,8 @@ func set_player_pos_to_curve_offset(offset : float, upDirection : Vector3 = Vect
 	var result = railPos + position
 	player.position = result
 	player.up_direction = upDirection
+	
+	DebugDraw3D.draw_sphere(result,0.15, Color.CHARTREUSE, 5.0)
 
 
 	
