89b6ac02359f654036978490077d2a989e1f4e56
Spyridon Passas <spyridonpassas0325@gmail.com>
2024-05-08 11:52:30 +0200

    Homing Attack Dash & Bounce Implemented.

--- scripts/gameplay/player/homing_attack.gd
+++ scripts/gameplay/player/homing_attack.gd
@@ -4,8 +4,12 @@ extends Node3D
 @onready var camera = $"../camera_anchor/camera"
 @onready var detection_area = $detection_area
 @onready var camera_anchor = $"../camera_anchor"
+@onready var player  : PlayerController = $".."
+
 
 @export var detectionAngle : float = 0.2
+@export var speed : float = 1.0
+@export var bounceHeight : float = 1.0
 
 var targetCollection : Array
 var currentHomingTarget : Node3D
@@ -16,8 +20,8 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	if not Input.is_action_just_pressed("attack"):
+func detect_homing_targets():
+	if not Input.is_action_just_pressed("attack") or player.is_on_floor():
 		return
 		
 	var copy = detection_area.get_overlapping_bodies()
@@ -35,6 +39,8 @@ func _physics_process(delta):
 		
 		if dot < detectionAngle:
 			continue
+		
+		DebugDraw3D.draw_sphere(entry.global_position, 1.15, Color.YELLOW)
 
 		var space_state = get_world_3d().direct_space_state
 		var query = PhysicsRayQueryParameters3D.create(global_position, global_position + dir * 1000)
@@ -48,7 +54,29 @@ func _physics_process(delta):
 		currentHomingTarget = col	
 		
 		DebugDraw3D.draw_sphere(col.global_position, 1.0, Color.RED, 2.0)
-		DebugDraw3D.draw_sphere(entry.global_position, 1.15, Color.YELLOW, 2.0)
 		
-		pass
+		player.set_default_controls(false)
+		
+
+func try_attack_target():
+	if currentHomingTarget == null:
+		return
+	
+	var dir = (currentHomingTarget.global_position - global_position).normalized()
+	var dist = (currentHomingTarget.global_position - global_position).length()
+	
+	if dist <= 0.5:
+		player.set_default_controls(true)
+		player.velocity = Vector3.UP * player.get_jump_velocity(bounceHeight)
+		player.move_and_slide()
+		currentHomingTarget = null
+		return
+		
+	player.velocity = dir * speed
+	player.move_and_slide()
+
+
+func _physics_process(delta):
+	detect_homing_targets()
+	try_attack_target()
 	
